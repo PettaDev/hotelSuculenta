548686d92e84006bb0b4fd4461906efc66fb1222
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2024-04-16 09:26:48 -0300

    Add files via upload

---
+++ Hotel/JS/script.js
@@ -0,0 +1,40 @@
+ /* Parte 1
+ . Valor da diária + qtd de dias de hospedagem
+ . Else "valor inválido" voltar function inicio
+ . If (diária > 0 &  0 < qtd de dias >= 30) - perguntar nome
+ . Else "valor inválido" voltar function inicio
+ . Depois de nome -> programa informa os quartos de 1 a 20
+ . Prompt p/ escolher, if- estiver ocupado "Quarto ocupado, escolha outro quarto"
+ . Else prompt "Digite CONFIRMAR, para confirmar a reserva ou VOLTAR para voltar ao menu inicial"
+*/
+
+/* Parte 2
+ . Qual o valor da diária?
+ . Nome e IDADE do hóspede - sem limite (while) até que informe "PARE"
+ . If - (idade < 6) não paga a diária - "[nome], hóspede não paga a diária"
+ . If - (idade >= 60) paga meia - "[nome], hóspede paga meia"
+ . Imprimir relatório {
+    -Quantidade de hóspede
+    -Quantidade de gratuintes
+    -Quantidade de meias
+    -Quantidade de normais (6 < idade < 60)
+    -Valor total
+ }
+*/
+
+/* Parte 3
+ . Cadastro de família;
+ . 1- Cadastrar; 2- Pesquisar; 3- Listar ; 4- Sair
+ //////////////
+ . 1- Cadastrar - terá um máx de 15 hóspede - gravando na memória - quando chegar a 15 -> "Limite máximo de cadastrado atingido"
+ . 2- Pesquisar - permite que procure nome dos hóspede:  obs: usar ARRAY
+    Se achar -> "Hóspede [nome], foi encontrado"
+    Se não -> "Hóspede [nome], não encontrado"
+ . 3- Listar - exibe todos os hóspedes do Hotel
+ . 4- Sair - usuário (funcionario do hotel), poderá mexer no programa (while) vezes - a não ser que coloque 4.
+*/
+
+/* Parte 4
+ . 
+ .
+*/
