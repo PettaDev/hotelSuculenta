35532321315023a1674dbc7e093e8f1b95cd78d0
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2025-01-01 15:15:49 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,6 @@ loginUserForm.addEventListener("submit", e => {
   senha = document.getElementById("userPassword").value; // Obtem o valor da senha do campo de entrada
 
   if (userName != "" && senha == "2678") { // Verifica se a senha inserida é igual à senha válida
-    alert(`Seja Bem-Vindo ${userName}`);
     window.location.href = "diaria.html";
   } else {
     alert("Usuário, login ou senha incorreto !");
