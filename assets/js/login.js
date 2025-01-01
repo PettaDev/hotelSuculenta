4b4cb5df6a344764b8f08508125a6ebf5b3631ed
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2025-01-01 15:15:20 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,7 +6,7 @@ loginUserForm.addEventListener("submit", e => {
   e.preventDefault();
 
   userName = document.getElementById("userName").value;
-  senha = document.getElementById("senha").value; // Obtem o valor da senha do campo de entrada
+  senha = document.getElementById("userPassword").value; // Obtem o valor da senha do campo de entrada
 
   if (userName != "" && senha == "2678") { // Verifica se a senha inserida é igual à senha válida
     alert(`Seja Bem-Vindo ${userName}`);
